64f5fbd5aa1dca20b230332ea39ecc099234750c
Nichalos <122895793@qq.com>
2019-01-14 17:00:30 +0800

    本地二次提交

--- movie/settings.py
+++ movie/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'movie.spiders'
 #USER_AGENT = 'movie (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 2
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3      # 下载延迟
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -64,8 +64,9 @@ DOWNLOAD_DELAY = 3
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+# 下面表示配置顺序， 按整数顺序执行。
 ITEM_PIPELINES = {
-   'movie.pipelines.MoviePipeline': 100,
+   'movie.pipelines.MoviePipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
